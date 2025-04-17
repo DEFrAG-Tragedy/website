64168c859158d9289623ab30f480a50a2e9dec44
Francesco Sessa <francesco.sessa@outlook.it>
2025-04-17 10:19:11 +0200

    added: logo durham

--- components/shared/Universities.vue
+++ components/shared/Universities.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="grid sm:grid-cols-2 md:grid-cols-4 gap-4 ">
-    <img src="/assets/images/logo_uniba.png" alt="Logo Uniba" class="object-contain w-full rounded-md" :class="logoHeight">
-    <img src="/assets/images/logo_unitr.png" alt="Logo Università Trento" class="object-contain w-full rounded-md" :class="logoHeight">
-    <img src="/assets/images/logo_units.png" alt="Logo Università Trieste" class="object-contain w-full rounded-md" :class="logoHeight">
-    <img src="/assets/images/logo_upo.png" alt="Logo Università del Piemonte Orientale" class="object-contain w-full rounded-md" :class="logoHeight">
+  <div class="grid sm:grid-cols-2 md:grid-cols-5 gap-4 ">
+    <a href="https://www.uniba.it"><img src="/assets/images/logo_uniba.png" alt="Logo Uniba" class="object-contain w-full rounded-md" :class="logoHeight"></a>
+    <a href="https://www.unitn.it"><img src="/assets/images/logo_unitr.png" alt="Logo Università Trento" class="object-contain w-full rounded-md" :class="logoHeight"></a>
+    <a href="https://www.units.it"><img src="/assets/images/logo_units.png" alt="Logo Università Trieste" class="object-contain w-full rounded-md" :class="logoHeight"></a>
+    <a href="https://www.uniupo.it"><img src="/assets/images/logo_upo.png" alt="Logo Università del Piemonte Orientale" class="object-contain w-full rounded-md" :class="logoHeight"></a>
+    <a href="https://www.durham.ac.uk"><img src="/assets/images/logo_durham.png" alt="Logo Durham University" class="object-contain w-full rounded-md" :class="logoHeight"></a>
   </div>
 </template>
 
